915f16d4cdb5b846a8fc76170360284b2d4d29fb
Ronnit Chopra <choprar3@msu.edu>
2025-02-12 23:40:20 -0500

    Please enter the commit message for your changes. Lines starting completetd assignment '#' will be ignored, and an empty message aborts the commit.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
 
 // The export statement makes these settings available to other files in 11ty
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("styles");
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("styles");
+
+    eleventyConfig.addShortcode(
+        "circle",
+        function (size, color) {
+            return `<div style="width:${size}px; height:${size}px; background-color:${color}; border-radius:50%;"></div>`;
+        });
+
+    eleventyConfig.addShortcode(
+        "card",
+        function (name, distance, color) {
+            return `<div class="card"><div class="logo" style="background: #${color};"></div>${name}<div class="distance">${distance} AU</div></div>`;
+        }
+    );
 };
